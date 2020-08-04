13e3eba37c2646c547c62b18bbe3ee9bdf7ef89e
zhang123 <feitizzz86@gmail.com>
2020-08-04 20:38:57 +0800

    Merge branch 'master' of https://github.com/zsqwer123/git-init
     Conflicts:
    	src/main/java/com/qf/Git.java

--- src/main/java/com/qf/Git.java
+++ src/main/java/com/qf/Git.java
@@ -2,6 +2,6 @@ package com.qf;
 
 public class Git {
     public static void main(String[] args) {
-        System.out.println("12345建勇你疯了吧++++++++++++！！！！！！！！！！" );
+        System.out.println("！！！！！！！" );
     }
 }
